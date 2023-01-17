b238cf0df399fad315ef4dbd6a13f7a5f275c112
willebill <wille0matheus@gmail.com>
2023-01-16 21:22:19 -0300

    10 - Hello World

--- src/main/java/io/github/willebill/VendasApplication.java
+++ src/main/java/io/github/willebill/VendasApplication.java
@@ -2,11 +2,19 @@ package io.github.willebill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello world!";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
-        System.out.println("Hello world!");
     }
 }

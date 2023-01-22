7fb0b9c4c8b8e8a76b8d32744fe4953124a702e2
willebill <wille0matheus@gmail.com>
2023-01-22 13:33:13 -0300

    16 - Trabalhando com múltiplos ambientes e profiles

--- src/main/java/io/github/willebill/MinhaConfiguration.java
+++ src/main/java/io/github/willebill/MinhaConfiguration.java
@@ -1,13 +1,22 @@
 package io.github.willebill;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
     @Bean(name = "applicationName")
     public String aplicationName() {
         return "Sistema de Vendas";
+    }
+
+    @Bean public CommandLineRunner executar(){
+        return  args -> {
+            System.out.println("Rodando a configuração de desenvolvimento.");
+        };
     }
 }
